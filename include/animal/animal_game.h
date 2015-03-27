044f005adda1a17392b0a4157716266d6fab02e1
Steve Shea <stevenlsjr@gmail.com>
2015-03-27 11:53:55 -0500

    removed c++-style comments and made headers c++ compatable

--- include/animal/animal_game.h
+++ include/animal/animal_game.h
@@ -6,10 +6,17 @@ for CMPS 1600, project 2
 #ifndef SLS_ANIMAL_GAME_H
 #define SLS_ANIMAL_GAME_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include "util.h"
 #include "btree.h"
 #include "animal.h"
+
+#ifdef __cplusplus
+}
+#endif
 
 
 #endif /* SLS_ANIMAL_GAME_H */
